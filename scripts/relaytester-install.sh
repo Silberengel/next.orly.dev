faad7ddc93f54eca4d110daae57649657e111913
mleku <me@mleku.dev>
2025-09-10 14:23:57 +0100

    add relay-tester scripts

---
+++ scripts/relaytester-install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+## rust must be installed
+if ! command -v "cargo" &> /dev/null; then
+    echo "rust and cargo is not installed."
+    echo "run this command to install:"
+    echo
+    echo "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh"
+    exit
+else
+    echo "cargo is installed."
+fi
+
+rm -rf relay-tester
+git clone https://github.com/mikedilger/relay-tester.git
+cd relay-tester
+cargo build -r
+cp target/release/relay-tester $GOBIN/
+cd ..
+rm -rf relay-tester
---
+++ scripts/relaytester-test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+## relay-tester must be installed
+if ! command -v "relay-tester" &> /dev/null; then
+    echo "relay-tester is not installed."
+    echo "run this command to install:"
+    echo
+    echo "./scripts/relaytester-install.sh"
+    exit
+fi
+export ORLY_LOG_LEVEL=trace
+export ORLY_LISTEN=127.0.0.1
+export ORLY_PORT=3334
+export ORLY_IP_WHITELIST=127.0.0
+export ORLY_ADMINS=nsec12l4072hvvyjpmkyjtdxn48xf8qj299zw60u7ddg58s2aphv3rpjqtg0tvr,nsec1syvtjgqauyeezgrev5nqrp36d87apjk87043tgu2usgv8umyy6wq4yl6tu
+go run . &
+sleep 2
+relay-tester ws://127.0.0.1:3334 nsec12l4072hvvyjpmkyjtdxn48xf8qj299zw60u7ddg58s2aphv3rpjqtg0tvr nsec1syvtjgqauyeezgrev5nqrp36d87apjk87043tgu2usgv8umyy6wq4yl6tu
+killall next.orly.dev
